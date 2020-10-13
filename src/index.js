03a8b66209b1452fd84a329ff5e6c54df71fb0f8
Denis <den_from_bykhov1988@mail.ru>
2020-10-13 20:30:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let strTmp = n.toString();
+  if (strTmp.startsWith('-')) strTmp = strTmp.slice(1);
+  if (strTmp.endsWith('0')) strTmp = strTmp.slice(0, -1);
+  return +strTmp.split("").reverse().join("");
 }
